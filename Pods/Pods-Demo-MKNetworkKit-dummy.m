71a2d495b96c66bb29313cbbf180a0b43212572d
Rex St John <rexstjohn@ux-rx.com>
2014-03-10 12:58:49 -0700

    fixing git ignores

--- Pods/Pods-Demo-MKNetworkKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Demo_MKNetworkKit : NSObject
-@end
-@implementation PodsDummy_Pods_Demo_MKNetworkKit
-@end
--- Pods/Pods-FourSquareKitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FourSquareKitTests : NSObject
-@end
-@implementation PodsDummy_Pods_FourSquareKitTests
-@end
