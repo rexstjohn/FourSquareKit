1c9a5429a61d6ca332196e18cc85300fc0bac78e
Rex St John <rexstjohn@ux-rx.com>
2014-03-10 17:32:06 -0700

    demo project stub, more ignores

---
+++ iOS-Demo/FourSquareKit/UXRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UXRAppDelegate.h
+//  FourSquareKit
+//
+//  Created by Rex St. John on 3/10/14.
+//  Copyright (c) 2014 UX-RX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UXRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
