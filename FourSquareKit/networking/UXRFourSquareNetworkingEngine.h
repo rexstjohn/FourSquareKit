8b6c5d10e2aefc9e1790c8dce98c1e63cd670277
JASON CROSS <jason.garnet.cross@gmail.com>
2015-03-06 19:04:50 -0500

    updating project to Xcode 6.1.1

--- FourSquareKit/networking/UXRFourSquareNetworkingEngine.h
+++ FourSquareKit/networking/UXRFourSquareNetworkingEngine.h
@@ -5,7 +5,7 @@
 //  Created by Rex St. John on 12/16/13.
 //  Copyright (c) 2013 UX-RX. All rights reserved.
 //
-#import "MKNetworkEngine.h"
+#import "MKNetworkKit.h"
 #import "UXRFourSquareRestaurantModel.h"
 #import <CoreLocation/CoreLocation.h>
 
